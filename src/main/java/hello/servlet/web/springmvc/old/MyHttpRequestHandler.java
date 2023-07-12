d56c8051beef082f4e87a36277278bc2f1a94734
ajroot5685 <anjeong8577@naver.com>
2023-07-12 21:06:34 +0900

    핸들러 매핑과 핸들러 어댑터

---
+++ src/main/java/hello/servlet/web/springmvc/old/MyHttpRequestHandler.java
@@ -0,0 +1,21 @@
+package hello.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component("/springmvc/request-handler")
+public class MyHttpRequestHandler implements HttpRequestHandler {
+    // localhost:8080/springmvc/request-handler 으로 실행하면 연결된다.
+    // 호출된 HandlerMapping : BeanNameUrlHandlerMapping
+    // 호출된 HandlerAdapter : HttpRequestHandlerAdapter
+
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MyHttpRequestHandler.handleRequest");
+    }
+}
