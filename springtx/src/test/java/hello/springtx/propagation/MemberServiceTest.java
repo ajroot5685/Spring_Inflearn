b1e3777e040bdf4d092569b78c3ba39ca7929ef9
ajroot5685 <anjeong8577@naver.com>
2024-04-03 11:02:19 +0900

    트랜잭션 전파 활용4 - 전파 커밋

--- springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -67,7 +67,25 @@ class MemberServiceTest {
     @Test
     void singleTx() {
         //given
-        String username = "outerTxOff_success";
+        String username = "singleTx";
+
+        //when
+        memberService.joinV1(username);
+
+        //then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * memberService    @Transational:ON
+     * memberRepository    @Transational:ON
+     * logRepository    @Transational:ON
+     */
+    @Test
+    void outerTxOn_success() {
+        //given
+        String username = "outerTxOn_success";
 
         //when
         memberService.joinV1(username);
