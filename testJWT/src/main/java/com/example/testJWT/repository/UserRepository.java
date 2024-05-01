5a881cada206b1cd9580bf793f9371778dd97e00
ajroot5685 <anjeong8577@naver.com>
2024-05-01 19:13:40 +0900

    회원가입 로직 구현

--- testJWT/src/main/java/com/example/testJWT/repository/UserRepository.java
+++ testJWT/src/main/java/com/example/testJWT/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.example.testJWT.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Boolean existsByUsername(String username);
 }
