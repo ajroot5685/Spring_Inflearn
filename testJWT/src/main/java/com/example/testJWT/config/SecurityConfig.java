a7b7913aabb351cf81a74cd49b8bc68774d7d082
ajroot5685 <anjeong8577@naver.com>
2024-05-01 21:04:18 +0900

    cors 설정

--- testJWT/src/main/java/com/example/testJWT/config/SecurityConfig.java
+++ testJWT/src/main/java/com/example/testJWT/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.testJWT.config;
 import com.example.testJWT.jwt.JWTFilter;
 import com.example.testJWT.jwt.JWTUtil;
 import com.example.testJWT.jwt.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,6 +15,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -44,6 +50,30 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors((corsCustomizer -> corsCustomizer.configurationSource(
+                        new CorsConfigurationSource() {
+
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                                CorsConfiguration configuration = new CorsConfiguration();
+                                //프론트엔트 서버 허용
+                                configuration.setAllowedOrigins(
+                                        Collections.singletonList("http://localhost:3000"));
+                                //허용하는 메소드
+                                configuration.setAllowedMethods(Collections.singletonList("*"));
+                                configuration.setAllowCredentials(true);
+                                //허용할 헤더
+                                configuration.setAllowedHeaders(Collections.singletonList("*"));
+                                //허용할 시간
+                                configuration.setMaxAge(3600L);
+                                //Authorizaion 헤더 허용
+                                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                                return configuration;
+                            }
+
+                        })));
 
         // csrf disable
         http
