3021a3d52f7429d620410bfeaa023fa10d1831ae
ajroot5685 <anjeong8577@naver.com>
2024-05-02 19:33:24 +0900

    프로젝트 생성 및 의존성 추가

---
+++ testOAuth2/src/main/java/com/example/testOAuth2/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.example.testOAuth2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class MyController {
+
+    @GetMapping("/my")
+    public String myPage() {
+        return "my";
+    }
+}
