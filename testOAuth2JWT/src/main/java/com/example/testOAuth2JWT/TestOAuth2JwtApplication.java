7969c0ecc84844ed1baa0946c106a86807182b2e
ajroot5685 <anjeong8577@naver.com>
2024-05-03 18:44:14 +0900

    프로젝트 생성 및 의존성 추가

---
+++ testOAuth2JWT/src/main/java/com/example/testOAuth2JWT/TestOAuth2JwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.testOAuth2JWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOAuth2JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestOAuth2JwtApplication.class, args);
+	}
+
+}
