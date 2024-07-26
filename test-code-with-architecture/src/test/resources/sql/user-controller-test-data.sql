95becc8ac6ac28244d828f832066bbde9fcfe68d
ajroot5685 <anjeong8577@naver.com>
2024-07-26 23:36:57 +0900

    userCreateController 테스트 코드 추가

---
+++ test-code-with-architecture/src/test/resources/sql/user-controller-test-data.sql
@@ -0,0 +1,4 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'kok202@naver.com', 'kok202', 'Seoul', 'aaaaaaaa-aaaaaaa-aaaa-aaaaaaaaa-aaaaaa', 'ACTIVE', 0);
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (2, 'kok203@naver.com', 'kok203', 'Seoul', 'aaaaaaaa-aaaaaaa-aaaa-aaaaaaaaa-aaaaab', 'PENDING', 0);
