135256377198ae69ab659cd2580850253b2fcb29
ajroot5685 <anjeong8577@naver.com>
2024-07-30 00:23:55 +0900

    패키지 구조 변경

--- test-code-with-architecture/src/test/java/com/example/demo/service/UserServiceTest.java
+++ test-code-with-architecture/src/test/java/com/example/demo/service/UserServiceTest.java
@@ -1,11 +1,12 @@
 package com.example.demo.service;
 
-import com.example.demo.exception.CertificationCodeNotMatchedException;
-import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.model.UserStatus;
-import com.example.demo.model.dto.UserCreateDto;
-import com.example.demo.model.dto.UserUpdateDto;
-import com.example.demo.repository.UserEntity;
+import com.example.demo.common.domain.exception.CertificationCodeNotMatchedException;
+import com.example.demo.common.domain.exception.ResourceNotFoundException;
+import com.example.demo.user.domain.UserCreate;
+import com.example.demo.user.domain.UserStatus;
+import com.example.demo.user.domain.UserUpdate;
+import com.example.demo.user.infrastructure.UserEntity;
+import com.example.demo.user.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -81,7 +82,7 @@ class UserServiceTest {
     @Test
     void UserCreateDto_를_이용하여_유저를_생성할_수_있다() {
         // given
-        UserCreateDto userCreateDto = UserCreateDto.builder()
+        UserCreate userCreateDto = UserCreate.builder()
                 .email("kok202@kakao.com")
                 .address("Gyeongi")
                 .nickname("kok202-k")
@@ -100,7 +101,7 @@ class UserServiceTest {
     @Test
     void UserUpdateDto_를_이용하여_유저를_수정할_수_있다() {
         // given
-        UserUpdateDto userUpdateDto = UserUpdateDto.builder()
+        UserUpdate userUpdateDto = UserUpdate.builder()
                 .address("Incheon")
                 .nickname("kok202-n")
                 .build();
