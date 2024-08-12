fc615541dd5f4efb5adcb3d975fe5137a0ede56d
ajroot5685 <anjeong8577@naver.com>
2024-08-12 22:18:01 +0900

    JPA를 이용한 Order 저장

---
+++ hellospring/src/main/java/tobyspring/hellospring/order/Order.java
@@ -0,0 +1,29 @@
+package tobyspring.hellospring.order;
+
+import jakarta.persistence.*;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "orders")
+@ToString
+public class Order {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String no;
+
+    private BigDecimal total;
+
+    public Order() {
+    }
+
+    public Order(String no, BigDecimal total) {
+        this.no = no;
+        this.total = total;
+    }
+}
