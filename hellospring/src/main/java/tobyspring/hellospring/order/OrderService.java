cabe2f6ef7b28f11860bcc0c66050d3e47630e44
ajroot5685 <anjeong8577@naver.com>
2024-08-13 23:13:56 +0900

    트랜잭션 서비스 추상화

--- hellospring/src/main/java/tobyspring/hellospring/order/OrderService.java
+++ hellospring/src/main/java/tobyspring/hellospring/order/OrderService.java
@@ -1,7 +1,7 @@
 package tobyspring.hellospring.order;
 
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 import tobyspring.hellospring.data.OrderJpaRepository;
 
@@ -11,9 +11,9 @@ import java.math.BigDecimal;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final JpaTransactionManager transactionManager;
+    private final PlatformTransactionManager transactionManager;
 
-    public OrderService(OrderJpaRepository orderRepository, JpaTransactionManager transactionManager) {
+    public OrderService(OrderJpaRepository orderRepository, PlatformTransactionManager transactionManager) {
         this.orderRepository = orderRepository;
         this.transactionManager = transactionManager;
     }
