7af0685469b9489b62b242ef4bb9b95dd5514766
ajroot5685 <anjeong8577@naver.com>
2024-08-14 23:43:34 +0900

    @Transactional과 AOP

--- hellospring/src/main/java/tobyspring/hellospring/order/OrderServiceImpl.java
+++ hellospring/src/main/java/tobyspring/hellospring/order/OrderServiceImpl.java
@@ -1,12 +1,14 @@
 package tobyspring.hellospring.order;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
