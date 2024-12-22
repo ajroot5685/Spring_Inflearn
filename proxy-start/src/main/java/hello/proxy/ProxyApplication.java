20047e2dd60e6cc8080f443d75f06f8c258786b2
ajroot5685 <anjeong8577@naver.com>
2024-12-22 14:58:50 +0900

    프록시 팩토리 - 적용1

--- proxy-start/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy-start/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,11 +1,11 @@
 package hello.proxy;
 
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(DynamicProxyFilterConfig.class)
+@Import(ProxyFactoryConfigV1.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
