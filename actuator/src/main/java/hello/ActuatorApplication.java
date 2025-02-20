d1c67a528ffb053c56b61309e702743804fbdf5b
ajroot5685 <anjeong8577@naver.com>
2025-02-20 16:36:58 +0900

    메트릭 등록5 - 게이지

--- actuator/src/main/java/hello/ActuatorApplication.java
+++ actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,5 +1,7 @@
 package hello;
 
+import hello.order.gauge.StockConfigV1;
+import hello.order.gauge.StockConfigV2;
 import hello.order.v1.OrderConfigV1;
 import hello.order.v2.OrderConfigV2;
 import hello.order.v3.OrderConfigV3;
@@ -10,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(OrderConfigV4.class)
+@Import({OrderConfigV4.class, StockConfigV2.class})
 @SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
