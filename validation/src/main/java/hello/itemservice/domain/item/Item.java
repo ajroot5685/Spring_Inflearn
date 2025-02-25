7f60b004102291d072e627afe152a8bd5e584204
ajroot5685 <anjeong8577@naver.com>
2025-02-25 17:41:02 +0900

    Bean Validation - 한계

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -9,6 +9,7 @@ import org.hibernate.validator.constraints.Range;
 @Data
 public class Item {
 
+    @NotNull // 수정 요구사항 추가
     private Long id;
 
     @NotBlank
@@ -19,7 +20,7 @@ public class Item {
     private Integer price;
 
     @NotNull
-    @Max(9999)
+//    @Max(9999) // 수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
