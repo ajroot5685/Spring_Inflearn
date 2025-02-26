be65c1a7e066a1e2b221655f313d89a7e464d405
ajroot5685 <anjeong8577@naver.com>
2025-02-26 12:44:00 +0900

    Bean Validation - HTTP 메시지 컨버터

---
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
@@ -0,0 +1,11 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.web.validation.form.ItemSaveForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
